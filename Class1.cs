a82972c09b551fe200e885e5b0a3fd822d4efec5
SinghNavneet93 <50494486+SinghNavneet93@users.noreply.github.com>
2019-05-28 20:24:44 -0400

    Add files via upload
    Get Length Test Case

---
+++ Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prog8170_Assignment1;
+using NUnit.Framework;
+
+
+namespace SENG8040_Assignment1_Test
+{
+    [TextFixture]
+
+    class SENG8040_Assignment1_Test
+    {
+        [Test]
+        public void GetLength_Input65_expectedLengthEquals65()
+        {
+
+            //Arrange
+            int length = 65;
+            int width = 45;
+
+            int expectedLength = length;
+
+            Rectangle testRectangle = new Rectangle(length, width);
+
+            //Act 
+            double actuallength = testRectangle.GetLength();
+
+            //Assert 
+            Assert.AreEqual(length, actuallength);
+        }
+    }
+}
+
+
